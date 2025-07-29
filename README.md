1fc456a7c13aee040d27d483d1aa9a31e7274935
Jeane <jeanecardoso.qa@gmail.com>
2025-07-28 21:43:23 -0300

    Adicionando funcionalidades de busca de piadas

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # chuck-norris-api
+
+Este projeto visa realizar chamadas para a API `https://api.chucknorris.io/` com as seguintes funcionalidades:
+
+- Listar uma piada aleatória
+- Listar uma piada aleatória de uma categoria específica
+- Listar todas as categorias
+
+Para o executar o projeto basta clonar o repositório e no terminal da pasta raiz inserir o comando: `node chuckNorris.js`.
+
+Feito por Jeane e Wemerson.
---
+++ chuckNorris.js
@@ -0,0 +1,55 @@
+const URL = 'https://api.chucknorris.io/jokes';
+
+async function getRandomJoke() {
+    try {
+            const response = await fetch(`${URL}/random`);
+            const joke = await response.json();
+            const value = joke.value;
+            return `Random joke: ${value}`;
+    } catch (error) {
+        console.log("Erro", error)
+    }
+}
+
+async function getRandomJokeByCategory(category) {
+    try {
+            const response = await fetch(`${URL}/random?category=${category}`);
+            const joke = await response.json();
+            const categories = joke.categories;
+            const randomJoke = joke.value;
+            return `Choosed category: ${categories}\nRandom joke: ${randomJoke}`;
+    } catch (error) {
+          console.log("Erro", error)
+    }
+}
+
+async function getAllCategories() {
+    try {
+            const response = await fetch(`${URL}/categories`);
+            const categories = await response.json();
+            categories.forEach((categories) => {
+                categories
+            });
+            return categories.join(', ');
+       
+    } catch (error) {
+          console.log("Erro", error)
+    }
+}
+
+async function main() {
+    const joke = await getRandomJoke();
+    const joke2 = await getRandomJokeByCategory("animal");
+    const joke3 = await getAllCategories();
+
+    console.log("Exibindo piada aleatória:")
+    console.log(joke)
+
+    console.log("\nExibindo categoria + piada aleatória:")
+    console.log(joke2)
+
+    console.log("\nCategorias")
+    console.log(joke3);
+}
+
+main();
